91bff9cc35f8b83e4d3b2274e39fc2bb5eb17a12
asif <asif69284@gmail.com>
2024-09-17 11:15:28 +0545

    Added required type

--- job-portal-server/Models/User.js
+++ job-portal-server/Models/User.js
@@ -2,9 +2,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    name: { type: String },
-    email: { type: String },
-    password: { type: String },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
 });
 
 const UserModel = mongoose.model('users', userSchema);
