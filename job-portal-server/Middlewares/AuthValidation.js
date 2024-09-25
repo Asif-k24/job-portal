9c2fd920ac4b69a367d22e56776f81c7de20d24b
asif <asif69284@gmail.com>
2024-09-25 11:44:48 +0545

    console logged error commented

--- job-portal-server/Middlewares/AuthValidation.js
+++ job-portal-server/Middlewares/AuthValidation.js
@@ -8,7 +8,7 @@ const signupValidation = (req, res, next) => {
     })
 
     const { error } = schema.validate(req.body);
-
+    console.log({error});
     if (error) {
         return res.status(400)
             .json({ message: "Bad Request", error })
