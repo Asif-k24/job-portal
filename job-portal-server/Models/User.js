f1c9a1e390ee610c06ac4728dc95ac1729009dfb
asif <asif69284@gmail.com>
2024-09-25 12:02:01 +0545

    UserModel

--- job-portal-server/Models/User.js
+++ job-portal-server/Models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+const UserSchema = new Schema({
     name: {
         type: String,
         required: true
@@ -16,5 +16,5 @@ const userSchema = new Schema({
     }
 });
 
-const UserModel = mongoose.model('users', userSchema);
+const UserModel = mongoose.model('users', UserSchema);
 module.exports = UserModel;
