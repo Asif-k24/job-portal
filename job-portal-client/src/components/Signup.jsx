d7b9ff182a2cf359fdab4c99c4ac4109deb1a77a
asif <asif69284@gmail.com>
2024-09-27 12:21:40 +0545

    Navbar component removed

--- job-portal-client/src/components/Signup.jsx
+++ job-portal-client/src/components/Signup.jsx
@@ -72,7 +72,7 @@ export default function Signup() {
 
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <div className=''>
         <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
