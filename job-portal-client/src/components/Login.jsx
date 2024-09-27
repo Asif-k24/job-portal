9af84d63638d0305e68a99b26f620d0bb39f960b
asif <asif69284@gmail.com>
2024-09-27 12:18:33 +0545

    Navbar component removed

--- job-portal-client/src/components/Login.jsx
+++ job-portal-client/src/components/Login.jsx
@@ -70,7 +70,6 @@ export default function Login() {
 
   return (
     <>
-      <Navbar />
       <div className=''>
         <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
