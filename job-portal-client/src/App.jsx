83936997fe23954499cf7d9d43d0e29745b445e2
asif <asif69284@gmail.com>
2024-09-28 22:44:13 +0545

    Modified with Routes and Router DOM using react router dom

--- job-portal-client/src/App.jsx
+++ job-portal-client/src/App.jsx
@@ -1,12 +1,31 @@
+import { Routes, Route } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import Navbar from './components/Navbar'
-import { Outlet } from 'react-router-dom'
+import Home from "./Pages/Home";
+import CreateJob from "./Pages/CreateJob";
+import MyJobs from "./Pages/MyJobs";
+import SalaryPage from "./Pages/SalaryPage";
+import UpdateJob from "./Pages/UpdateJob";
+import Login from "./components/Login";
+import JobDetails from "./Pages/JobDetails";
+import Signup from "./components/Signup";
 import './App.css'
 
 function App() {
   return (
     <>
       <Navbar />
-      <Outlet />
+      <Routes>
+        <Route path='/' element={<Navigate to='/auth/login' /> && <Login />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/post-job' element={<CreateJob />} />
+        <Route path='/my-job' element={<MyJobs />} />
+        <Route path='/salary' element={<SalaryPage />} />
+        <Route path='/edit-job/:id' element={<UpdateJob />} loader={({ params }) => fetch(`http://localhost:3000/all-jobs/${params.id}`)} />
+        <Route path='/job/:id' element={<JobDetails />} />
+        <Route path='/auth/login' element={<Login />} />
+        <Route path='/auth/signup' element={<Signup />} />
+      </Routes>
     </>
   )
 }
