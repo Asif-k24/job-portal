6f7a97b69a482aa55215d9b98c39f6d1d0ab9c1c
asif <asif69284@gmail.com>
2024-09-28 22:49:34 +0545

    typo fixed

--- job-portal-client/src/components/Banner.jsx
+++ job-portal-client/src/components/Banner.jsx
@@ -7,7 +7,7 @@ export default function Banner({ query, handleInputChange }) {
         <div className='max-w-screen-2xl container mx-auto xl:px-24 px-4 md:py-20 py 14'>
             <h1 className="text-5xl font-bold text-primary mb-3">Find your
                 <span className='text-blue'> new job</span> today</h1>
-            <p className='text-lg text-black/70 mb-8'>Thousands of jobs in the computer, engineering and technolgy sectors are waiting for you.</p>
+            <p className='text-lg text-black/70 mb-8'>Thousands of jobs in the computer, engineering and technology sectors are waiting for you.</p>
 
             <form>
                 <div className='flex justify-start md:flex-row flex-col md:gap-0 gap-4'>
