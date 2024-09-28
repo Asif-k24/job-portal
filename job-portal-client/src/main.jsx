cb0d395d03bf3204ef568f4f4de1c065ebc39c31
asif <asif69284@gmail.com>
2024-09-28 22:41:23 +0545

    modified main with browser router

--- job-portal-client/src/main.jsx
+++ job-portal-client/src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import router from './Router/Router'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
+// import { RouterProvider } from 'react-router-dom'
+// import router from './Router/Router'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
