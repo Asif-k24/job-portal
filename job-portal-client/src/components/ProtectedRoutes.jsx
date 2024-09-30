bd223aec82b2f5a8d88e139c02bac527077e47e5
asif <asif69284@gmail.com>
2024-09-30 12:08:33 +0545

    Modified with jwtToken logic

--- job-portal-client/src/components/ProtectedRoutes.jsx
+++ job-portal-client/src/components/ProtectedRoutes.jsx
@@ -1,12 +1,17 @@
-import React, { Outlet, Navigate } from 'react-router-dom'
+import React from 'react';
+import { Outlet, Navigate } from 'react-router-dom'
+import { handleError } from '../utils';
 
 export default function ProtectedRoutes() {
 
-    const user = null;
+    const user = localStorage.getItem('token');
+    const errorMsg = "Login Required"
 
     return (
         <div>
-            user ? <Outlet /> : <Navigate to="/auth/login" />
+            {
+                user ? <Outlet /> : <Navigate to="/" /> && handleError(errorMsg)
+            }
         </div>
     )
 }
