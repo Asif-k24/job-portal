aa72cc25e9352049724f47441b924cf6bac6cc0a
asif <asif69284@gmail.com>
2024-09-30 12:06:59 +0545

    Implemented protected routes for children component

--- job-portal-client/src/App.jsx
+++ job-portal-client/src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 import Navbar from './components/Navbar'
@@ -9,6 +10,7 @@ import UpdateJob from "./Pages/UpdateJob";
 import Login from "./components/Login";
 import JobDetails from "./Pages/JobDetails";
 import Signup from "./components/Signup";
+import ProtectedRoutes from "./components/ProtectedRoutes";
 import './App.css'
 
 function App() {
@@ -17,14 +19,16 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Navigate to='/auth/login' /> && <Login />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/post-job' element={<CreateJob />} />
-        <Route path='/my-job' element={<MyJobs />} />
-        <Route path='/salary' element={<SalaryPage />} />
-        <Route path='/edit-job/:id' element={<UpdateJob />} loader={({ params }) => fetch(`http://localhost:3000/all-jobs/${params.id}`)} />
-        <Route path='/job/:id' element={<JobDetails />} />
-        <Route path='/auth/login' element={<Login />} />
         <Route path='/auth/signup' element={<Signup />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path='/home' element={<Home />} />
+          <Route path='/post-job' element={<CreateJob />} />
+          <Route path='/my-job' element={<MyJobs />} />
+          <Route path='/salary' element={<SalaryPage />} />
+          <Route path='/edit-job/:id' element={<UpdateJob />} loader={({ params }) => fetch(`http://localhost:3000/all-jobs/${params.id}`)} />
+          <Route path='/job/:id' element={<JobDetails />} />
+          <Route path='/auth/login' element={<Login />} />
+        </Route>
       </Routes>
     </>
   )
